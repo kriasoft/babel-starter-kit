c11fd7996e70d2fdb9f20bdb8135034f39f87541
Igor Suvorov <suvorov.igor.90@gmail.com>
2016-11-05 11:39:15 +0400

    npm run dev

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-/**
- * Babel Starter Kit (https://www.kriasoft.com/babel-starter-kit)
- *
- * Copyright © 2015-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-export Greeting from './Greeting.js';
+function hello(name) {
+  console.log(`Hello ${name}`); // eslint-disable-line
+}
+hello('JS World');
